442ad79169255f84e0fa73c41c2b23d0ea5342a9
Nafikovtt <nafikovtt@gmail.com>
2024-02-25 16:53:09 +0500

    HW_solved

--- src/main/kotlin/task3_main.kt
+++ src/main/kotlin/task3_main.kt
@@ -5,16 +5,19 @@ fun main() {
     val price = Scanner(System.`in`).nextInt()
     println("Вы постоянный покупатель? Введите 'Да' или 'Нет'")
     val answer = readln()
-    var discontedPrice = 0
-    val constantCustomer = if (answer.lowercase() == "да") true else false
-    if (price in 1..1000) {
-        discontedPrice = if (constantCustomer) (price * 0.99).toInt() else price
-        println("Стоимость покупки составляет: $discontedPrice рублей.")
-    } else if (price in 1001..10_000) {
-        discontedPrice = if (constantCustomer) ((price -100) * 0.99).toInt() else price - 100
-        println("Стоимость покупки составляет: $discontedPrice рублей.")
-    } else if (price in 10_001..Int.MAX_VALUE ){
-        discontedPrice = if (constantCustomer) Math.round(((price * 0.95) * 0.99)).toInt() else (price * 0.95).toInt()
-        println("Стоимость покупки составляет: $discontedPrice рублей.")
+    val constantCustomer = answer.lowercase() == "да"
+    when (price) {
+        in 1..1000 -> {
+            val discontedPrice = if (constantCustomer) (price * 0.99).toInt() else price
+            println("Стоимость покупки составляет: $discontedPrice рублей.")
+        }
+        in 1001..10_000 -> {
+            val discontedPrice = if (constantCustomer) ((price -100) * 0.99).toInt() else price - 100
+            println("Стоимость покупки составляет: $discontedPrice рублей.")
+        }
+        in 10_001..Int.MAX_VALUE -> {
+            val discontedPrice = if (constantCustomer) Math.round(((price * 0.95) * 0.99)).toInt() else (price * 0.95).toInt()
+            println("Стоимость покупки составляет: $discontedPrice рублей.")
+        }
     }
 }
