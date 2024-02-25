9c0f438791a7f79ff02064e3734fe6c222b09de0
Nafikovtt <nafikovtt@gmail.com>
2024-02-25 19:33:33 +0500

    Корректировака второй задачи

--- src/main/kotlin/task2_main.kt
+++ src/main/kotlin/task2_main.kt
@@ -3,7 +3,7 @@ import java.util.Scanner
 fun main() {
     println("Введите количество лайков: ")
     val likes = Scanner(System.`in`).nextInt()
-    if (likes % 2 == 1 && likes != 11) {
+    if ((likes % 10 == 1 && likes % 100 != 11) || likes == 1) {
         println("Понравилось $likes человеку")
     } else {
         println("Понравилось $likes людям")
